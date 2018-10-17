fbe37f8d2c5fa3895be640c92c9df97b60f15996
Vigilans <vigilans@foxmail.com>
2018-10-17 21:51:59 +0800

    update charset and lexicographical compare

--- Lexer/utility.hpp
+++ Lexer/utility.hpp
@@ -17,13 +17,46 @@ template <char c>
 constexpr static auto ch = char_constant<c>{};
 
 template <char... chars>
-struct char_sequence {
+struct char_sequence : std::integer_sequence<char, chars...> {
     template <size_t i>
     constexpr static decltype(auto) get() {
         static_assert(i < sizeof...(chars));
         return char_constant<std::get<i>(std::make_tuple(chars...))>{};
     }
 };
+
+template <char... chars>
+struct char_set {
+    template <char ch>
+    constexpr static bool has = ((chars == ch) || ...);
+
+    template <char ch>
+    using insert = std::conditional_t<has<ch>, char_set<chars...>, char_set<ch, chars...>>;
+};
+
+template <class Left, class Right>
+struct concat;
+
+// 按照less-than字典序进行比较
+template <class T, class I1, class I2>
+struct lex_compare;
+
+template <class T> // 两个空序列比较返回false
+struct lex_compare<T, std::integer_sequence<T>, std::integer_sequence<T>> : std::false_type {};
+
+template <class T, T v, T... rest> // 非空序列比空序列大，返回false
+struct lex_compare<T, std::integer_sequence<T, v, rest...>, std::integer_sequence<T>> : std::false_type {};
+
+template <class T, T v, T... rest> // 空序列比非空序列小，返回true
+struct lex_compare<T, std::integer_sequence<T>, std::integer_sequence<T, v, rest...>> : std::true_type {};
+
+template <class T, T v1, T... I1, T v2, T... I2> // 两者均非空时，进行默认字典序比较
+struct lex_compare<T, std::integer_sequence<T, v1, I1...>, std::integer_sequence<T, v2, I2...>>
+    : std::bool_constant<
+        v1 < v2 ? true : 
+        v1 > v2 ? false :
+        lex_compare<T, std::integer_sequence<T, I1...>, std::integer_sequence<T, I2...>>::value
+    >;
 
 template <bool v, class T1, class T2>
 constexpr decltype(auto) op_cond(std::bool_constant<v>, T1 lhs, T2 rhs) {
